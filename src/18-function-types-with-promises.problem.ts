82b756ecfbdf770d7594d17fb19c6f8a29789df8
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-12-17 15:27:29 -0500

    lesson 18

--- src/18-function-types-with-promises.problem.ts
+++ src/18-function-types-with-promises.problem.ts
@@ -1,4 +1,4 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest';
 
 interface User {
   id: string;
@@ -7,8 +7,8 @@ interface User {
 }
 
 const createThenGetUser = async (
-  createUser: unknown,
-  getUser: unknown,
+  createUser: () => Promise<string>,
+  getUser: (id: string) => Promise<User>
 ): Promise<User> => {
   const userId: string = await createUser();
 
@@ -17,19 +17,19 @@ const createThenGetUser = async (
   return user;
 };
 
-it("Should create the user, then get them", async () => {
+it('Should create the user, then get them', async () => {
   const user = await createThenGetUser(
-    async () => "123",
+    async () => '123',
     async (id) => ({
       id,
-      firstName: "Matt",
-      lastName: "Pocock",
-    }),
+      firstName: 'Matt',
+      lastName: 'Pocock',
+    })
   );
 
   expect(user).toEqual({
-    id: "123",
-    firstName: "Matt",
-    lastName: "Pocock",
+    id: '123',
+    firstName: 'Matt',
+    lastName: 'Pocock',
   });
 });
